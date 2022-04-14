41f5bf3d7aa6bcf3b5c91e84ef3442b8cba9321b
master_jiho <aksjajsxm123@gmail.com>
2022-04-14 14:27:00 +0900

    adding basic settings

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+
+app.listen(8080, function(){
+    console.log('listening on 8080');
+});
+
+app.get('/index', function(요청, 응답){
+    응답.send('반갑습니다');
+});
+
+app.get('/beauty', (req, res)=> res.send('뷰티상품 진열소'));
+
+app.get('/', (req,res)=> res.sendFile(__dirname+'/index.html'));
+
