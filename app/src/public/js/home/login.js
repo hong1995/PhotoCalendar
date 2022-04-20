85ca40c5197004d3218310d2e45db79a541ab6fb
master_jiho <aksjajsxm123@gmail.com>
2022-04-20 21:30:38 +0900

    로그인기능 간단구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,14 @@
 
-console.log("hello");
+const id = document.querySelector("#id");
+const password = document.querySelector("#pswd");
+const submit = document.querySelector("button");
+// const form = document.querySelector("form");
+
+submit.addEventListener("click", login);
+function login(){
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+}
